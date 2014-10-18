3219aa18d4a541bee06637d8f54484c089fcffaf
Alex Broad <alexsbroad@gmail.com>
2014-10-17 19:03:48 -0500

    final commit, contains updated code with verified velocity values and bag file

--- src/turtle_tf_broadcaster.py
+++ src/turtle_tf_broadcaster.py
@@ -6,36 +6,46 @@ import math
 import tf
 from geometry_msgs.msg import Twist
 from turtlesim.srv import TeleportAbsolute
+from turtlesim.msg import Pose
+import sys
 
-def talker():
-     # set initial state to (x,y,theta) = (0,0,0)
-     #rospy.wait_for_service('turtle1/teleport_absolute')
-     #turtle1_teleport = rospy.ServiceProxy('turtle1/teleport_absolute', TeleportAbsolute)
-     #turtle1_teleport(0,0,0)
-
-     __rate_T = 10 # this should be a private parameter
+def talker(rate_T):
+     # set initial position
+     rospy.wait_for_service('turtle1/teleport_absolute')
+     turtle1_teleport = rospy.ServiceProxy('turtle1/teleport_absolute', TeleportAbsolute)
+     turtle1_teleport(5.4444444, 5.4444444,(math.pi)/6.5)
+     
      pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
-     rospy.init_node('turtle_tf_broadcaster')
-     r = rospy.Rate(__rate_T)
-     test = Twist()
-     test.linear.x = 0.0
-     test.linear.y = 0.0
-     test.linear.z = 0.0
-     test.angular.x = 0.0
-     test.angular.y = 0.0
-     test.angular.z = 0.0
+     velocity = Twist()
+     start_time = rospy.get_time()
      while not rospy.is_shutdown():
-          now = rospy.get_time()
-          cur_angle = math.atan2(3*math.sin((4*math.pi*now)/__rate_T),3*math.sin((2*math.pi*now)/__rate_T))
-          test.linear.x = 3*math.sin((4*math.pi*now)/__rate_T) * math.cos(cur_angle)
-          #test.linear.y = 3*math.sin((2*math.pi*now)/rate_T)
-          test.angular.z = math.atan2(3*math.sin((4*math.pi*now)/__rate_T),3*math.sin((2*math.pi*now)/__rate_T))
-          pub.publish(test)
-          r.sleep()
+          now = rospy.get_time() - start_time
+
+          x_in = (4*math.pi*now)/rate_T
+          y_in = (2*math.pi*now)/rate_T
+
+          x_t = 3*math.sin(x_in)
+          x_t_1 = ((12*math.pi)/rate_T) * math.cos(x_in)
+          x_t_2 = -((48*(math.pow(math.pi,2)))/(math.pow(rate_T,2)))*math.sin(x_in)
+
+          y_t = 3*math.sin(y_in)
+          y_t_1 = ((6*math.pi)/rate_T) * math.cos(y_in)
+          y_t_2 = -((12*(math.pow(math.pi,2)))/(math.pow(rate_T,2)))*math.sin(y_in)
+
+          cur_angle = math.atan2(y_t,x_t)
+
+          v_t = math.sqrt(math.pow(x_t_1,2) + math.pow(y_t_1,2))
+          w_t = ((x_t_1*y_t_2) - (y_t_1*x_t_2))/(math.pow(x_t_1,2) + math.pow(y_t_1,2))
+
+          velocity.linear.x = v_t #* math.cos(cur_angle)
+          velocity.angular.z = w_t#cur_angle
+
+          pub.publish(velocity)
 
 if __name__ == '__main__':
+     args = rospy.myargv(argv=sys.argv)
+     rate_T = int(args[1])
      try:
-          talker()
+          rospy.init_node('turtle_tf_broadcaster')
+          talker(rate_T) 
      except rospy.ROSInterruptException: pass
-
-
